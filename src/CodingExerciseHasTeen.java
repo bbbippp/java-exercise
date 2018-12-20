e602c2666ff2841809160a415e593481994805a0
Ian Gumilang <ian.gomu@gmail.com>
2018-12-20 14:37:57 +0700

    add answer to hasTeen

--- src/CodingExerciseHasTeen.java
+++ src/CodingExerciseHasTeen.java
@@ -3,5 +3,16 @@
  */
 public class CodingExerciseHasTeen
 {
+    public static boolean hasTeen(int firstParam, int secondParam, int thirdParam)
+    {
+        boolean result = false;
+
+        if ((firstParam >= 13 && firstParam <= 19) || (secondParam >= 13 && secondParam <= 19) || (thirdParam >= 13 && thirdParam <= 19))
+        {
+            result = true;
+        }
+
+        return result;
+    }
 
 }
